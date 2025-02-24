67141fd9e9f2847b7617dbed1844ec263440bc04
Abin <trashwbin@gmail.com>
2025-02-24 15:41:51 +0800

    feat(product): 重构数据库和缓存初始化逻辑
    - 修改数据库初始化，使用环境变量配置连接信息
    - 添加自动迁移功能和连接池配置
    - 更新 Redis 初始化逻辑，使用环境变量配置连接信息
    - 修改服务名称和地址配置
    - 更新 go.mod 文件，添加新依赖

--- app/product/main.go
+++ app/product/main.go
@@ -7,14 +7,22 @@ import (
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	"github.com/joho/godotenv"
 	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
+	consul "github.com/kitex-contrib/registry-consul"
+	"github.com/trashwbin/dymall/app/product/biz/dal"
 	"github.com/trashwbin/dymall/app/product/conf"
+	"github.com/trashwbin/dymall/app/product/infra/rpc"
 	"github.com/trashwbin/dymall/rpc_gen/kitex_gen/product/productcatalogservice"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
+	_ = godotenv.Load()
+	dal.Init()
+	rpc.InitClient()
+
 	opts := kitexInit()
 
 	svr := productcatalogservice.NewServer(new(ProductCatalogServiceImpl), opts...)
@@ -37,7 +45,12 @@ func kitexInit() (opts []server.Option) {
 	opts = append(opts, server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
 		ServiceName: conf.GetConf().Kitex.Service,
 	}))
-
+	// 加载consul注册中心
+	r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		klog.Fatal(err)
+	}
+	opts = append(opts, server.WithRegistry(r))
 	// klog
 	logger := kitexlogrus.NewLogger()
 	klog.SetLogger(logger)
